ab0e5476bc598a55025b9903b0c8a4f49f49ffc4
Michael Chadwick <michael.chadwick@gmail.com>
2014-05-31 23:50:48 -0700

    added functionality and simple test

--- lib/lita/handlers/crazycaps.rb
+++ lib/lita/handlers/crazycaps.rb
@@ -1,6 +1,28 @@
+require "lita"
+
 module Lita
   module Handlers
     class Crazycaps < Handler
+      route(/^crazycaps\s+(.+)/i, :crazycaps, command: true, help: {
+        "crazycaps PHRASE" => "Return PHRASE as a randomly capitalized new phrase."
+      })
+      
+      def crazycaps(response)
+        phrase = response.matches[0][0]
+        phrase_cc = ""
+        
+        phrase.each_char do |c|
+          r = Random.new.rand(0..1)
+          if (r == 0)
+            c = c.upcase
+          else
+            c = c.downcase
+          end
+          phrase_cc += c
+        end
+        
+        response.reply(phrase_cc)
+      end
     end
 
     Lita.register_handler(Crazycaps)
--- spec/lita/handlers/crazycaps_spec.rb
+++ spec/lita/handlers/crazycaps_spec.rb
@@ -1,4 +1,12 @@
 require "spec_helper"
 
 describe Lita::Handlers::Crazycaps, lita_handler: true do
+  it { routes_command("crazycaps phrase").to(:crazycaps) }
+  
+  describe "#crazycaps" do
+    it "responds with crazycapped PHRASE" do
+      send_command("crazycaps hello")
+      expect(replies.last.downcase).to eq("hello")
+    end
+  end
 end
